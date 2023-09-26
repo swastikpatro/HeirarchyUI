a50c97ee7a17c8d5169bd4b933a9acb3d295a0f0
swastikpatro <patroswastik.jsdev@gmail.com>
2023-09-26 14:28:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box as="div">
+      <Text>Hello world</Text>
+    </Box>
+  );
+}
+
+export default App;
